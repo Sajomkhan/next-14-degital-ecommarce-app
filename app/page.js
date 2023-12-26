b8f9a07b11ff152a8d89941d44188081a2077935
Saiful Ajom Khan <sajomkhan@gmail.com>
2023-12-27 00:52:48 +0600

    updated

--- app/page.js
+++ app/page.js
@@ -20,23 +20,6 @@ export default function Home() {
     }
   }
 
-  const rightArrow = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-      />
-    </svg>
-  );
-
   return (
     <main>
       <Hero />
@@ -45,7 +28,7 @@ export default function Home() {
         className="bg-primary text-gray-100 p-4 px-6 text-lg rounded-lg hover:bg-primaryDeep shadow-lg"
       >
         <div className="flex gap-2 items-center align-middle">
-          Login With Google {rightArrow}
+          Login With Google
         </div>
       </button>
     </main>
